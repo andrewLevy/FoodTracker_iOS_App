e32909273b3a6e8d49cc3225ab23ea0b92963cca
andrewLevy <levya@bu.edu>
2016-09-29 23:26:40 -0400

    Added Default Label Func

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
     }
-
-
 }
 
